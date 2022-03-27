aba7ab14b428176ec5477ea93a01ca6afb3cc161
constantinoi <pescaalma@gmail.com>
2022-03-27 20:20:50 -0300

    filtro de usuário

---
+++ src/main/java/br/com/cursojsf/filter/FilterAutenticacao.java
@@ -0,0 +1,50 @@
+package br.com.cursojsf.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import br.com.cursojsf.jpautil.JPAUtil;
+
+@WebFilter(urlPatterns = { "/*" })
+public class FilterAutenticacao implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("Invocando");
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+
+		String usuarioLogado = (String) session.getAttribute("usuarioLogado");
+
+		String url = req.getServletPath();
+
+		if (!url.equalsIgnoreCase("login.xhtml") && usuarioLogado == null
+				|| (usuarioLogado != null && usuarioLogado.trim().isEmpty())) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/login.xhtml");
+			dispatcher.forward(request, response);
+			return;
+		} else {
+			// executa acores do request e do response
+			chain.doFilter(request, response);
+
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		JPAUtil.getEntityManager();
+	}
+}
