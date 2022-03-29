f9f8a0d5381e866ec918a875c4d728c0863fa171
constantinoi <pescaalma@gmail.com>
2022-03-28 22:05:51 -0300

    Mostrando e ocultando de acordo com o perfil do usuário

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -76,7 +76,7 @@ public class PessoaBean {
 
 			FacesContext context = FacesContext.getCurrentInstance();
 			ExternalContext externalContext = context.getExternalContext();
-			externalContext.getSessionMap().put("usuarioLogado", pessoaUser.getLogin());
+			externalContext.getSessionMap().put("usuarioLogado", pessoaUser);
 
 //			HttpServletRequest req = (HttpServletRequest) externalContext.getRequest();
 //			HttpSession session = req.getSession();
@@ -87,6 +87,18 @@ public class PessoaBean {
 		}
 
 		return "login.xhtml";
+	}
+
+	public boolean permiteAcesso(String acesso) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = context.getExternalContext();
+		
+		HttpServletRequest req = (HttpServletRequest) externalContext.getRequest();
+		HttpSession session = req.getSession();
+		
+		Pessoa pessoaUser = (Pessoa) session.getAttribute("usuarioLogado");
+		
+		return pessoaUser.getPerfilUser().equals(acesso);
 	}
 
 }
